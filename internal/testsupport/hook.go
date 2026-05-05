c9c61b8066c7ade5e5aece3401e96ff6973c2c22
Jason Pearson <jason.d.pearson@gmail.com>
2026-05-05 17:37:40 -0500

    testsupport: copy hook plan results

--- internal/testsupport/hook.go
+++ internal/testsupport/hook.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"sync"
 
+	"github.com/kaeawc/grit/internal/graph"
 	"github.com/kaeawc/grit/internal/integration"
 )
 
@@ -30,7 +31,7 @@ func (h *HookRecorder) BeforePlan(ctx context.Context, req integration.PlanReque
 func (h *HookRecorder) AfterPlan(ctx context.Context, result integration.PlanResult, model integration.ReadOnlyModel) error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	h.After = append(h.After, result)
+	h.After = append(h.After, clonePlanResult(result))
 	if h.AfterFn != nil {
 		return h.AfterFn(ctx, result, model)
 	}
@@ -48,5 +49,31 @@ func (h *HookRecorder) BeforeSnapshot() []integration.PlanRequest {
 func (h *HookRecorder) AfterSnapshot() []integration.PlanResult {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	return append([]integration.PlanResult(nil), h.After...)
+	results := make([]integration.PlanResult, len(h.After))
+	for i, result := range h.After {
+		results[i] = clonePlanResult(result)
+	}
+	return results
+}
+
+func clonePlanResult(result integration.PlanResult) integration.PlanResult {
+	result.Variants = append([]string(nil), result.Variants...)
+	result.Actions = append([]graph.Action(nil), result.Actions...)
+	for i, action := range result.Actions {
+		result.Actions[i] = cloneAction(action)
+	}
+	return result
+}
+
+func cloneAction(action graph.Action) graph.Action {
+	action.Inputs = append([]graph.ArtifactID(nil), action.Inputs...)
+	action.Outputs = append([]graph.ArtifactID(nil), action.Outputs...)
+	if action.Attributes != nil {
+		attributes := make(map[string]string, len(action.Attributes))
+		for key, value := range action.Attributes {
+			attributes[key] = value
+		}
+		action.Attributes = attributes
+	}
+	return action
 }
